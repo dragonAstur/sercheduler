2b2626d7189ff1cb8c93b0a8055aae2faf1924d8
Pablo Barredo Gil <pablo.barredo@grupotsk.com>
2022-11-25 13:46:49 +0100

    Prepare create computation matrix

---
+++ src/platform.cc
@@ -0,0 +1,24 @@
+#include "sercheduler/platform.h"
+
+namespace sercheduler {
+
+std::vector<std ::vector<double>> CalculateComputationMatrix(
+    const std::vector<Task>& tasks, const std::vector<Host>& hosts,
+    int reference_flops) {
+  std::vector<std ::vector<double>> computation_matrix(
+      tasks.size(), std::vector<double>(hosts.size()));
+
+  for (const auto& task : tasks) {
+    for (const auto& host : hosts) {
+      float flops_factor =
+          float(reference_flops) / float(host.flops * host.cores);
+      // Multiply the runtime by the factor to know the runtime with the new
+      // hardware
+      computation_matrix[task.id][host.id] = task.runtime * flops_factor;
+    }
+  }
+
+  return computation_matrix;
+}
+
+}  // namespace sercheduler
---
+++ test/platform_test.cc
@@ -0,0 +1,2 @@
+#include "gtest/gtest.h"
+#include "sercheduler/platform.h"
