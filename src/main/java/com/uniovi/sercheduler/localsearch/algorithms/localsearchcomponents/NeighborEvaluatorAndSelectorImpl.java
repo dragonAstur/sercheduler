7e25e895926bc56b6294b2babf676017bbbc7e11
Pelayo González García <uo276559@uniovi.es>
2025-10-17 14:14:02 +0200

    Localsearch - adding a component which mixes both LSA steps generation and selection

---
+++ src/main/java/com/uniovi/sercheduler/localsearch/algorithms/localsearchcomponents/NeighborEvaluatorAndSelectorImpl.java
@@ -0,0 +1,80 @@
+package com.uniovi.sercheduler.localsearch.algorithms.localsearchcomponents;
+
+import com.uniovi.sercheduler.jmetal.problem.SchedulePermutationSolution;
+import com.uniovi.sercheduler.localsearch.evaluator.LocalsearchEvaluator;
+import com.uniovi.sercheduler.localsearch.observer.Observer;
+import com.uniovi.sercheduler.localsearch.operator.*;
+import com.uniovi.sercheduler.service.FitnessInfo;
+import com.uniovi.sercheduler.service.PlanPair;
+
+import java.util.*;
+
+public class NeighborEvaluatorAndSelectorImpl {
+
+    private final NeighborSelector selector;
+
+    public NeighborEvaluatorAndSelectorImpl() {
+        this.selector = new NeighborSelectorImpl();
+    }
+
+
+    public SchedulePermutationSolution generateAndEvaluateNeighbors(List<NeighborhoodOperatorGlobal> neighborhoodOperatorList,
+                                                           SchedulePermutationSolution actualSolution,
+                                                           TerminationCriterion terminationCriterion,
+                                                           LocalsearchEvaluator evaluator,
+                                                           Observer observer){
+
+        List<SchedulePermutationSolution> generatedSolutions = new ArrayList<>();
+
+        for(NeighborhoodOperatorGlobal globalOperator : neighborhoodOperatorList){
+
+            generatedSolutions.add(
+                    generateAndEvaluateGlobal(actualSolution, globalOperator, terminationCriterion, evaluator, observer)
+            );
+
+            if(terminationCriterion.hasTimeExceeded()) break;
+        }
+
+        return generatedSolutions.stream()
+                .min(Comparator.comparingDouble(s -> s.getFitnessInfo().fitness().get("makespan")))
+                .orElseThrow(() -> new NoSuchElementException("No hay soluciones generadas"));
+
+    }
+
+    private SchedulePermutationSolution generateAndEvaluateGlobal(SchedulePermutationSolution actualSolution,
+                                                              NeighborhoodOperatorGlobal globalOperator,
+                                                              TerminationCriterion terminationCriterion,
+                                                              LocalsearchEvaluator evaluator,
+                                                              Observer observer){
+
+        List<PlanPair> plan = List.copyOf(actualSolution.getPlan());
+        List<GeneratedNeighbor> positionalGeneratedNeighbors;
+
+        SchedulePermutationSolution actualBestNeighbor, totalBestNeighbor = null;
+        NeighborhoodOperatorPositional positionalOperator = globalOperator.getNeighborhoodOperatorPositional();
+
+        for(int i = 0; i < plan.size(); i++) {
+
+            positionalGeneratedNeighbors = positionalOperator.execute(actualSolution, i);
+
+            //Select the best neighbors from all neighbors generated in that position
+            actualBestNeighbor = this.selector.selectBestNeighborGlobal(actualSolution,
+                    positionalGeneratedNeighbors, evaluator, terminationCriterion);
+
+            if(totalBestNeighbor == null || totalBestNeighbor.getFitnessInfo().fitness().get("makespan") > actualBestNeighbor.getFitnessInfo().fitness().get("makespan"))
+                    totalBestNeighbor = actualBestNeighbor;
+
+            if(terminationCriterion.hasTimeExceeded())
+                break;
+        }
+
+        this.selector.updateObserverMetrics(observer);
+
+        return totalBestNeighbor;
+
+    }
+
+
+
+
+}
